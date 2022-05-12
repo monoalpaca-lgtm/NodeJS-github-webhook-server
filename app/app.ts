49091fa2c5fa6f45ef8034aa512bc029e4a752d2
Andy Chen <borshiuan.chen@gmail.com>
2022-05-12 00:57:33 -0400

    try payload

--- app/app.ts
+++ app/app.ts
@@ -39,6 +39,7 @@ app.get('/pullGit', (req, res) => {
 
 app.post('/github-webhooks/payload', (req, res) => {
   console.log('Got body:', req.body);
+  console.log('Got payload:', req.payload);
   res.sendStatus(200);
   res.end();
 
